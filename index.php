14e5d20f46e12b61162a6867782fb25267cf5195
Moppi <33616102+moppi79@users.noreply.github.com>
2018-12-30 19:38:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $month = date('n',time());
 $year = date('Y',time());
 
 
-if (--$month == 0){
+if (--$month == 0){ ## Is the month bevor in the last year
   
   $back_year = $year - 1;
   $back_month = 12;
@@ -15,7 +15,7 @@ if (--$month == 0){
   
 }
 
-if ($month++ == 13){
+if ($month++ == 13){ # is next month in the new year
   $vor_year = $year + 1;
   $vor_month = 1;
   
@@ -29,7 +29,7 @@ if ($month++ == 13){
 
 <html>
 <head>
-<script src='java.js' type='text/javascript'></script>
+<script src='java.js' type='text/javascript'></script> <!-- Include The JS Funktions  -->
 <style>
 
 
@@ -37,15 +37,15 @@ if ($month++ == 13){
 </head>
 
 
-<body onload="abfrage('<?php print $month;?>','<?php print $year;?>');"> 
+<body onload="abfrage('<?php print $month;?>','<?php print $year;?>');"> <!-- To load the JS-Funktions on Start -->
 
-<div id='Ansicht'></div>
-
-
-<div id='vor'><button onclick="abfrage('<?php print $vor_month;?>','<?php print $vor_year;?>');" >vor</button></div>
+<div id='Ansicht'></div> <!-- Main Div  -->
 
 
-<div id='back'><button onclick="abfrage('<?php print $back_month;?>','<?php print $back_year;?>');" >back</button></div>
+<div id='vor'><button onclick="abfrage('<?php print $vor_month;?>','<?php print $vor_year;?>');" >vor</button></div> <!-- Next month Div  -->
+
+
+<div id='back'><button onclick="abfrage('<?php print $back_month;?>','<?php print $back_year;?>');" >back</button></div> <!-- Last month Div -->
 
 </body>
 </html>

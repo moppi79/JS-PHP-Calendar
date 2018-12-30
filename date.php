d6c3c0455feeaf4e0b0f253ce3f2eb1591da7ea7
Moppi <33616102+moppi79@users.noreply.github.com>
2018-12-30 19:34:21 +0100

    Update date.php

--- date.php
+++ date.php
@@ -1,12 +1,12 @@
 <?php
 
-include('date_class.php');
+include('date_class.php'); #Import Class
 
 
-$test = new calender_create();
+$test = new calender_create(); #Create new Objekt
 
 
-$test->start_date($_POST['month'],$_POST['year']);
+$test->start_date($_POST['month'],$_POST['year']); #Submit Month and Year
 
 
 $test->week_nr(1); #week on/off
@@ -15,18 +15,22 @@ $test->week_day(1); #week_day on/off
 
 $test->month_switch(1); #month on/off
 
-$test->data_insert('table','background-color: a400ff;color: 4f006f; border: 1px solid black; border-collapse: collapse;',''); #define Month CSS
-$test->data_insert('year','background-color: a400ff;color: 4f006f; padding: 15px; border-collapse: collapse; border: 1px solid black;',''); #define Month CSS
-$test->data_insert('day','background-color: f2f222;color: 4f006f; padding: 15px; border-collapse: collapse; border: 1px solid black',''); #your Standart Daily
-$test->data_insert('overlap','background-color: d3d3d3;color: 4f006f; padding: 15px; border-collapse: collapse; border: 1px solid black',''); #define Month overlapping
-$test->data_insert('week','background-color: a80375;color: 4f006f; padding: 15px; border-collapse: collapse; border: 1px solid black',''); # name of day or 
+$test->data_insert('table','background-color: a400ff;color: 4f006f; border: 1px solid black; border-collapse: collapse;',''); #Define Table Border, Global Table data
+
+$test->data_insert('year','background-color: a400ff;color: 4f006f; padding: 15px; border-collapse: collapse; border: 1px solid black;',''); #Month Year Header
+$test->data_insert('day','background-color: f2f222;color: 4f006f; padding: 15px; border-collapse: collapse; border: 1px solid black',''); #Every day cell
+$test->data_insert('overlap','background-color: d3d3d3;color: 4f006f; padding: 15px; border-collapse: collapse; border: 1px solid black',''); #Overlapping Days 
+$test->data_insert('week','background-color: a80375;color: 4f006f; padding: 15px; border-collapse: collapse; border: 1px solid black',''); # Weekday´s and number of week
 
 
+#### data_insert(DAY,CSS DATA,VALUE);
+#### DAY --> d[day][month] --> d53 is the 5.3.xxxx 
 
+#### You can all data fill with a DB call or simular
 
 $test->data_insert("d6".$_POST['month']."",'background-color: ffffff;color: 4f006f;','');
 $test->data_insert("d9".$_POST['month']."",'background-color: ffffff;color: 4f006f;','ggg');
 
-print (json_encode($test->Calendar()));
+print (json_encode($test->Calendar())); #call all data und send zu JS 
 
 ?>
